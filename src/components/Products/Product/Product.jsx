453bd163807e5e20a370c84f2d7cc2e5b951ffe5
nexzanavod <aanavod@gmail.com>
2021-06-16 04:26:31 +0530

    first commit

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -15,33 +15,31 @@ const Product = ({ product }) => {
   const classes = useStyles();
 
   return (
-    <div>
-      <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image={product.image}
-          title={product.name}
-        />
-        <CardContent>
-          <div className={classes.cardContent}>
-            <Typography variant="h5" gutterBottom>
-              {product.name}
-            </Typography>
-            <Typography variant="h5" gutterBottom>
-              {product.price}
-            </Typography>
-          </div>
-          <Typography variant="body2" color="textSecondary">
-            {product.description}
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.name}
+      />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
           </Typography>
-        </CardContent>
-        <CardActions disableSpacing className={classes.cardAction}>
-          <IconButton aria-label="Add to Cart">
-            <AddShoppingCart />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </div>
+          <Typography variant="h5" gutterBottom>
+            {product.price}
+          </Typography>
+        </div>
+        <Typography variant="body2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardAction}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
